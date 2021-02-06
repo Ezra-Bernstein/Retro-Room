2de2cf77540867c7e77dff07a06d9929fcb1474f
Ezra Bernstein <mremdb@gmail.com>
2021-02-06 13:46:15 -0500

    Image Recognition
    got image recognition working on /upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from vision import get_labels
 
 app = Flask(__name__)
 
@@ -8,6 +9,27 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method=='POST':
+        if 'file' not in request.files:
+                return "NO FILE"
+        file = request.files['file']
+        filename = "image.jpg"
+        file.save('/tmp/' + filename)
+        labels = get_labels('/tmp/' + filename)
+        classification=''
+        classification = labels[0].description
+                
+        if classification=='':
+            classification="no labels found"
 
-# if __name__ == '__main__':
-#     app.run(host='127.0.0.1', port=8080, debug=True)
+        return render_template('display_labels.html', labels=labels, classification=classification)
+    elif request.method=='GET':
+        return render_template('upload.html')
+    
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
